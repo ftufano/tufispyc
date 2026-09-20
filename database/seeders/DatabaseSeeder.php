807714a0347982b8588384fe7f8ffadef0ba2aa0
Francesco Nicola Tufano Lugo <francescotufano@hotmail.com>
2026-09-20 16:42:12 -0400

    v1.0 spanish translate and seeder mod

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::firstOrCreate(
+        User::updateOrCreate(
             ['email' => config('menu.admin_email')],
             [
                 'name' => 'Admin',
